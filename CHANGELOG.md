f0ddef75ed238cb40dc5c058a2b669d52348797a
Jonathan Hartman <j@p4nt5.com>
2015-04-26 20:09:39 -0700

    Initial commit; freshly-generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Kindle Cookbook CHANGELOG
+=========================
+
+v0.0.1 (2015-04-26)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'kindle'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures kindle'
+long_description 'Installs/Configures kindle'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
