4c461e1bb8d4a6963be096cc10668f2c826a783b
Jonathan Hartman <j@p4nt5.com>
2015-05-11 15:44:08 -0700

    Try getting rid of some of the hacky cookbook stubs

---
+++ spec/support/cookbooks/dmg/metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'dmg'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures dmg'
+long_description 'Installs/Configures dmg'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
