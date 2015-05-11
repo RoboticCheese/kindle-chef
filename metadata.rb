33b557e8642e419b9ab7014297ed21009daeb26b
Jonathan Hartman <j@p4nt5.com>
2015-05-10 22:27:25 -0700

    Bump the mac-app-store dep

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ description      'Installs/Configures the Kindle app'
 long_description 'Installs/Configures the Kindle app'
 version          '0.0.1'
 
-depends          'mac-app-store', '~> 0.1'
+depends          'mac-app-store', '~> 1.0'
 
 supports         'mac_os_x'
 # rubocop:enable SingleSpaceBeforeFirstArg
