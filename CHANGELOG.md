c036edefb030072ea4f2f57929e3a375fee445b3
Jonathan Hartman <j@p4nt5.com>
2015-05-11 20:29:44 -0700

    Bump version for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 Kindle Cookbook CHANGELOG
 =========================
 
+v0.1.0 (2015-05-11)
+-------------------
+- Initial release! OS X (App Store or direct) and Windows (direct) support!
+
 v0.0.1 (2015-04-26)
 -------------------
 - Development started
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'j@p4nt5.com'
 license          'apache2'
 description      'Installs/Configures the Kindle app'
 long_description 'Installs/Configures the Kindle app'
-version          '0.0.1'
+version          '0.1.0'
 
 depends          'mac-app-store', '~> 1.0'
 depends          'dmg', '~> 2.2'
